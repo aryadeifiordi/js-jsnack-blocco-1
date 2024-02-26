02f9e104ce93d9c7e529055e0da1062d6eb86397
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-26 10:50:03 +0100

    init

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -1,4 +1,8 @@
 const firstWord = prompt ('type first')
 const secondWord = prompt ('type second')
 
-console.log
+console.log(firstWord, secondWord)
+
+if (firstWord.length > secondWord.length){
+    console.log(`Shortest word: ${secondWord} - longer word: ${firstWord}`);
+} else if 
